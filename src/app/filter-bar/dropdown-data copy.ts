0ef205e5d6fc414ed9b1448482434b661bc0f443
Adil Mae <itsadilhadid@gmail.com>
2024-08-07 01:03:55 +0100

    added year and country filters

---
+++ src/app/filter-bar/dropdown-data copy.ts
@@ -0,0 +1,959 @@
+export const genres = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+];
+
+export const languages = [
+  {
+    iso: 'xx',
+    english_name: 'No Language',
+    name: 'No Language',
+  },
+  {
+    iso: 'aa',
+    english_name: 'Afar',
+    name: '',
+  },
+  {
+    iso: 'af',
+    english_name: 'Afrikaans',
+    name: 'Afrikaans',
+  },
+  {
+    iso: 'ak',
+    english_name: 'Akan',
+    name: '',
+  },
+  {
+    iso: 'an',
+    english_name: 'Aragonese',
+    name: '',
+  },
+  {
+    iso: 'as',
+    english_name: 'Assamese',
+    name: '',
+  },
+  {
+    iso: 'av',
+    english_name: 'Avaric',
+    name: '',
+  },
+  {
+    iso: 'ae',
+    english_name: 'Avestan',
+    name: '',
+  },
+  {
+    iso: 'ay',
+    english_name: 'Aymara',
+    name: '',
+  },
+  {
+    iso: 'az',
+    english_name: 'Azerbaijani',
+    name: 'Azərbaycan',
+  },
+  {
+    iso: 'ba',
+    english_name: 'Bashkir',
+    name: '',
+  },
+  {
+    iso: 'bm',
+    english_name: 'Bambara',
+    name: 'Bamanankan',
+  },
+  {
+    iso: 'bi',
+    english_name: 'Bislama',
+    name: '',
+  },
+  {
+    iso: 'bo',
+    english_name: 'Tibetan',
+    name: '',
+  },
+  {
+    iso: 'br',
+    english_name: 'Breton',
+    name: '',
+  },
+  {
+    iso: 'ca',
+    english_name: 'Catalan',
+    name: 'Català',
+  },
+  {
+    iso: 'cs',
+    english_name: 'Czech',
+    name: 'Český',
+  },
+  {
+    iso: 'ce',
+    english_name: 'Chechen',
+    name: '',
+  },
+  {
+    iso: 'cu',
+    english_name: 'Slavic',
+    name: '',
+  },
+  {
+    iso: 'cv',
+    english_name: 'Chuvash',
+    name: '',
+  },
+  {
+    iso: 'kw',
+    english_name: 'Cornish',
+    name: '',
+  },
+  {
+    iso: 'co',
+    english_name: 'Corsican',
+    name: '',
+  },
+  {
+    iso: 'cr',
+    english_name: 'Cree',
+    name: '',
+  },
+  {
+    iso: 'cy',
+    english_name: 'Welsh',
+    name: 'Cymraeg',
+  },
+  {
+    iso: 'da',
+    english_name: 'Danish',
+    name: 'Dansk',
+  },
+  {
+    iso: 'de',
+    english_name: 'German',
+    name: 'Deutsch',
+  },
+  {
+    iso: 'dv',
+    english_name: 'Divehi',
+    name: '',
+  },
+  {
+    iso: 'dz',
+    english_name: 'Dzongkha',
+    name: '',
+  },
+  {
+    iso: 'eo',
+    english_name: 'Esperanto',
+    name: 'Esperanto',
+  },
+  {
+    iso: 'et',
+    english_name: 'Estonian',
+    name: 'Eesti',
+  },
+  {
+    iso: 'eu',
+    english_name: 'Basque',
+    name: 'euskera',
+  },
+  {
+    iso: 'fo',
+    english_name: 'Faroese',
+    name: '',
+  },
+  {
+    iso: 'fj',
+    english_name: 'Fijian',
+    name: '',
+  },
+  {
+    iso: 'fi',
+    english_name: 'Finnish',
+    name: 'suomi',
+  },
+  {
+    iso: 'fr',
+    english_name: 'French',
+    name: 'Français',
+  },
+  {
+    iso: 'fy',
+    english_name: 'Frisian',
+    name: '',
+  },
+  {
+    iso: 'ff',
+    english_name: 'Fulah',
+    name: 'Fulfulde',
+  },
+  {
+    iso: 'gd',
+    english_name: 'Gaelic',
+    name: '',
+  },
+  {
+    iso: 'ga',
+    english_name: 'Irish',
+    name: 'Gaeilge',
+  },
+  {
+    iso: 'gl',
+    english_name: 'Galician',
+    name: 'Galego',
+  },
+  {
+    iso: 'gv',
+    english_name: 'Manx',
+    name: '',
+  },
+  {
+    iso: 'gn',
+    english_name: 'Guarani',
+    name: '',
+  },
+  {
+    iso: 'gu',
+    english_name: 'Gujarati',
+    name: '',
+  },
+  {
+    iso: 'ht',
+    english_name: 'Haitian; Haitian Creole',
+    name: '',
+  },
+  {
+    iso: 'ha',
+    english_name: 'Hausa',
+    name: 'Hausa',
+  },
+  {
+    iso: 'sh',
+    english_name: 'Serbo-Croatian',
+    name: '',
+  },
+  {
+    iso: 'hz',
+    english_name: 'Herero',
+    name: '',
+  },
+  {
+    iso: 'ho',
+    english_name: 'Hiri Motu',
+    name: '',
+  },
+  {
+    iso: 'hr',
+    english_name: 'Croatian',
+    name: 'Hrvatski',
+  },
+  {
+    iso: 'hu',
+    english_name: 'Hungarian',
+    name: 'Magyar',
+  },
+  {
+    iso: 'ig',
+    english_name: 'Igbo',
+    name: '',
+  },
+  {
+    iso: 'io',
+    english_name: 'Ido',
+    name: '',
+  },
+  {
+    iso: 'ii',
+    english_name: 'Yi',
+    name: '',
+  },
+  {
+    iso: 'iu',
+    english_name: 'Inuktitut',
+    name: '',
+  },
+  {
+    iso: 'ie',
+    english_name: 'Interlingue',
+    name: '',
+  },
+  {
+    iso: 'ia',
+    english_name: 'Interlingua',
+    name: '',
+  },
+  {
+    iso: 'id',
+    english_name: 'Indonesian',
+    name: 'Bahasa indonesia',
+  },
+  {
+    iso: 'ik',
+    english_name: 'Inupiaq',
+    name: '',
+  },
+  {
+    iso: 'is',
+    english_name: 'Icelandic',
+    name: 'Íslenska',
+  },
+  {
+    iso: 'it',
+    english_name: 'Italian',
+    name: 'Italiano',
+  },
+  {
+    iso: 'jv',
+    english_name: 'Javanese',
+    name: '',
+  },
+  {
+    iso: 'ja',
+    english_name: 'Japanese',
+    name: '日本語',
+  },
+  {
+    iso: 'kl',
+    english_name: 'Kalaallisut',
+    name: '',
+  },
+  {
+    iso: 'kn',
+    english_name: 'Kannada',
+    name: '?????',
+  },
+  {
+    iso: 'ks',
+    english_name: 'Kashmiri',
+    name: '',
+  },
+  {
+    iso: 'kr',
+    english_name: 'Kanuri',
+    name: '',
+  },
+  {
+    iso: 'kk',
+    english_name: 'Kazakh',
+    name: 'қазақ',
+  },
+  {
+    iso: 'km',
+    english_name: 'Khmer',
+    name: '',
+  },
+  {
+    iso: 'ki',
+    english_name: 'Kikuyu',
+    name: '',
+  },
+  {
+    iso: 'rw',
+    english_name: 'Kinyarwanda',
+    name: 'Kinyarwanda',
+  },
+  {
+    iso: 'ky',
+    english_name: 'Kirghiz',
+    name: '??????',
+  },
+  {
+    iso: 'kv',
+    english_name: 'Komi',
+    name: '',
+  },
+  {
+    iso: 'kg',
+    english_name: 'Kongo',
+    name: '',
+  },
+  {
+    iso: 'ko',
+    english_name: 'Korean',
+    name: '한국어/조선말',
+  },
+  {
+    iso: 'kj',
+    english_name: 'Kuanyama',
+    name: '',
+  },
+  {
+    iso: 'ku',
+    english_name: 'Kurdish',
+    name: '',
+  },
+  {
+    iso: 'lo',
+    english_name: 'Lao',
+    name: '',
+  },
+  {
+    iso: 'la',
+    english_name: 'Latin',
+    name: 'Latin',
+  },
+  {
+    iso: 'lv',
+    english_name: 'Latvian',
+    name: 'Latviešu',
+  },
+  {
+    iso: 'li',
+    english_name: 'Limburgish',
+    name: '',
+  },
+  {
+    iso: 'ln',
+    english_name: 'Lingala',
+    name: '',
+  },
+  {
+    iso: 'lt',
+    english_name: 'Lithuanian',
+    name: 'Lietuvių',
+  },
+  {
+    iso: 'lb',
+    english_name: 'Letzeburgesch',
+    name: '',
+  },
+  {
+    iso: 'lu',
+    english_name: 'Luba-Katanga',
+    name: '',
+  },
+  {
+    iso: 'lg',
+    english_name: 'Ganda',
+    name: '',
+  },
+  {
+    iso: 'mh',
+    english_name: 'Marshall',
+    name: '',
+  },
+  {
+    iso: 'ml',
+    english_name: 'Malayalam',
+    name: '',
+  },
+  {
+    iso: 'mr',
+    english_name: 'Marathi',
+    name: '',
+  },
+  {
+    iso: 'mg',
+    english_name: 'Malagasy',
+    name: '',
+  },
+  {
+    iso: 'mt',
+    english_name: 'Maltese',
+    name: 'Malti',
+  },
+  {
+    iso: 'mo',
+    english_name: 'Moldavian',
+    name: '',
+  },
+  {
+    iso: 'mn',
+    english_name: 'Mongolian',
+    name: '',
+  },
+  {
+    iso: 'mi',
+    english_name: 'Maori',
+    name: '',
+  },
+  {
+    iso: 'ms',
+    english_name: 'Malay',
+    name: 'Bahasa melayu',
+  },
+  {
+    iso: 'my',
+    english_name: 'Burmese',
+    name: '',
+  },
+  {
+    iso: 'na',
+    english_name: 'Nauru',
+    name: '',
+  },
+  {
+    iso: 'nv',
+    english_name: 'Navajo',
+    name: '',
+  },
+  {
+    iso: 'nr',
+    english_name: 'Ndebele',
+    name: '',
+  },
+  {
+    iso: 'nd',
+    english_name: 'Ndebele',
+    name: '',
+  },
+  {
+    iso: 'ng',
+    english_name: 'Ndonga',
+    name: '',
+  },
+  {
+    iso: 'ne',
+    english_name: 'Nepali',
+    name: '',
+  },
+  {
+    iso: 'nl',
+    english_name: 'Dutch',
+    name: 'Nederlands',
+  },
+  {
+    iso: 'nn',
+    english_name: 'Norwegian Nynorsk',
+    name: '',
+  },
+  {
+    iso: 'nb',
+    english_name: 'Norwegian Bokmål',
+    name: 'Bokmål',
+  },
+  {
+    iso: 'no',
+    english_name: 'Norwegian',
+    name: 'Norsk',
+  },
+  {
+    iso: 'ny',
+    english_name: 'Chichewa; Nyanja',
+    name: '',
+  },
+  {
+    iso: 'oc',
+    english_name: 'Occitan',
+    name: '',
+  },
+  {
+    iso: 'oj',
+    english_name: 'Ojibwa',
+    name: '',
+  },
+  {
+    iso: 'or',
+    english_name: 'Oriya',
+    name: '',
+  },
+  {
+    iso: 'om',
+    english_name: 'Oromo',
+    name: '',
+  },
+  {
+    iso: 'os',
+    english_name: 'Ossetian; Ossetic',
+    name: '',
+  },
+  {
+    iso: 'pi',
+    english_name: 'Pali',
+    name: '',
+  },
+  {
+    iso: 'pl',
+    english_name: 'Polish',
+    name: 'Polski',
+  },
+  {
+    iso: 'pt',
+    english_name: 'Portuguese',
+    name: 'Português',
+  },
+  {
+    iso: 'qu',
+    english_name: 'Quechua',
+    name: '',
+  },
+  {
+    iso: 'rm',
+    english_name: 'Raeto-Romance',
+    name: '',
+  },
+  {
+    iso: 'ro',
+    english_name: 'Romanian',
+    name: 'Română',
+  },
+  {
+    iso: 'rn',
+    english_name: 'Rundi',
+    name: 'Kirundi',
+  },
+  {
+    iso: 'ru',
+    english_name: 'Russian',
+    name: 'Pусский',
+  },
+  {
+    iso: 'sg',
+    english_name: 'Sango',
+    name: '',
+  },
+  {
+    iso: 'sa',
+    english_name: 'Sanskrit',
+    name: '',
+  },
+  {
+    iso: 'si',
+    english_name: 'Sinhalese',
+    name: 'සිංහල',
+  },
+  {
+    iso: 'sk',
+    english_name: 'Slovak',
+    name: 'Slovenčina',
+  },
+  {
+    iso: 'sl',
+    english_name: 'Slovenian',
+    name: 'Slovenščina',
+  },
+  {
+    iso: 'se',
+    english_name: 'Northern Sami',
+    name: '',
+  },
+  {
+    iso: 'sm',
+    english_name: 'Samoan',
+    name: '',
+  },
+  {
+    iso: 'sn',
+    english_name: 'Shona',
+    name: '',
+  },
+  {
+    iso: 'sd',
+    english_name: 'Sindhi',
+    name: '',
+  },
+  {
+    iso: 'so',
+    english_name: 'Somali',
+    name: 'Somali',
+  },
+  {
+    iso: 'st',
+    english_name: 'Sotho',
+    name: '',
+  },
+  {
+    iso: 'es',
+    english_name: 'Spanish',
+    name: 'Español',
+  },
+  {
+    iso: 'sq',
+    english_name: 'Albanian',
+    name: 'shqip',
+  },
+  {
+    iso: 'sc',
+    english_name: 'Sardinian',
+    name: '',
+  },
+  {
+    iso: 'sr',
+    english_name: 'Serbian',
+    name: 'Srpski',
+  },
+  {
+    iso: 'ss',
+    english_name: 'Swati',
+    name: '',
+  },
+  {
+    iso: 'su',
+    english_name: 'Sundanese',
+    name: '',
+  },
+  {
+    iso: 'sw',
+    english_name: 'Swahili',
+    name: 'Kiswahili',
+  },
+  {
+    iso: 'sv',
+    english_name: 'Swedish',
+    name: 'svenska',
+  },
+  {
+    iso: 'ty',
+    english_name: 'Tahitian',
+    name: '',
+  },
+  {
+    iso: 'ta',
+    english_name: 'Tamil',
+    name: 'தமிழ்',
+  },
+  {
+    iso: 'tt',
+    english_name: 'Tatar',
+    name: '',
+  },
+  {
+    iso: 'te',
+    english_name: 'Telugu',
+    name: 'తెలుగు',
+  },
+  {
+    iso: 'tg',
+    english_name: 'Tajik',
+    name: '',
+  },
+  {
+    iso: 'tl',
+    english_name: 'Tagalog',
+    name: '',
+  },
+  {
+    iso: 'th',
+    english_name: 'Thai',
+    name: 'ภาษาไทย',
+  },
+  {
+    iso: 'ti',
+    english_name: 'Tigrinya',
+    name: '',
+  },
+  {
+    iso: 'to',
+    english_name: 'Tonga',
+    name: '',
+  },
+  {
+    iso: 'tn',
+    english_name: 'Tswana',
+    name: '',
+  },
+  {
+    iso: 'ts',
+    english_name: 'Tsonga',
+    name: '',
+  },
+  {
+    iso: 'tk',
+    english_name: 'Turkmen',
+    name: '',
+  },
+  {
+    iso: 'tr',
+    english_name: 'Turkish',
+    name: 'Türkçe',
+  },
+  {
+    iso: 'tw',
+    english_name: 'Twi',
+    name: '',
+  },
+  {
+    iso: 'ug',
+    english_name: 'Uighur',
+    name: '',
+  },
+  {
+    iso: 'uk',
+    english_name: 'Ukrainian',
+    name: 'Український',
+  },
+  {
+    iso: 'ur',
+    english_name: 'Urdu',
+    name: 'اردو',
+  },
+  {
+    iso: 'uz',
+    english_name: 'Uzbek',
+    name: 'ozbek',
+  },
+  {
+    iso: 've',
+    english_name: 'Venda',
+    name: '',
+  },
+  {
+    iso: 'vi',
+    english_name: 'Vietnamese',
+    name: 'Tiếng Việt',
+  },
+  {
+    iso: 'vo',
+    english_name: 'Volapük',
+    name: '',
+  },
+  {
+    iso: 'wa',
+    english_name: 'Walloon',
+    name: '',
+  },
+  {
+    iso: 'wo',
+    english_name: 'Wolof',
+    name: 'Wolof',
+  },
+  {
+    iso: 'xh',
+    english_name: 'Xhosa',
+    name: '',
+  },
+  {
+    iso: 'yi',
+    english_name: 'Yiddish',
+    name: '',
+  },
+  {
+    iso: 'za',
+    english_name: 'Zhuang',
+    name: '',
+  },
+  {
+    iso: 'zu',
+    english_name: 'Zulu',
+    name: 'isiZulu',
+  },
+  {
+    iso: 'ab',
+    english_name: 'Abkhazian',
+    name: '',
+  },
+  {
+    iso: 'zh',
+    english_name: 'Mandarin',
+    name: '普通话',
+  },
+  {
+    iso: 'ps',
+    english_name: 'Pushto',
+    name: 'پښتو',
+  },
+  {
+    iso: 'am',
+    english_name: 'Amharic',
+    name: '',
+  },
+  {
+    iso: 'ar',
+    english_name: 'Arabic',
+    name: 'العربية',
+  },
+  {
+    iso: 'bg',
+    english_name: 'Bulgarian',
+    name: 'български език',
+  },
+  {
+    iso: 'cn',
+    english_name: 'Cantonese',
+    name: '广州话 / 廣州話',
+  },
+  {
+    iso: 'mk',
+    english_name: 'Macedonian',
+    name: '',
+  },
+  {
+    iso: 'el',
+    english_name: 'Greek',
+    name: 'ελληνικά',
+  },
+  {
+    iso: 'fa',
+    english_name: 'Persian',
+    name: 'فارسی',
+  },
+  {
+    iso: 'he',
+    english_name: 'Hebrew',
+    name: 'עִבְרִית',
+  },
+  {
+    iso: 'hi',
+    english_name: 'Hindi',
+    name: 'हिन्दी',
+  },
+  {
+    iso: 'hy',
+    english_name: 'Armenian',
+    name: '',
+  },
+  {
+    iso: 'en',
+    english_name: 'English',
+    name: 'English',
+  },
+  {
+    iso: 'ee',
+    english_name: 'Ewe',
+    name: 'Èʋegbe',
+  },
+  {
+    iso: 'ka',
+    english_name: 'Georgian',
+    name: 'ქართული',
+  },
+  {
+    iso: 'pa',
+    english_name: 'Punjabi',
+    name: 'ਪੰਜਾਬੀ',
+  },
+  {
+    iso: 'bn',
+    english_name: 'Bengali',
+    name: 'বাংলা',
+  },
+  {
+    iso: 'bs',
+    english_name: 'Bosnian',
+    name: 'Bosanski',
+  },
+  {
+    iso: 'ch',
+    english_name: 'Chamorro',
+    name: "Finu' Chamorro",
+  },
+  {
+    iso: 'be',
+    english_name: 'Belarusian',
+    name: 'беларуская мова',
+  },
+  {
+    iso: 'yo',
+    english_name: 'Yoruba',
+    name: 'Èdè Yorùbá',
+  },
+];
