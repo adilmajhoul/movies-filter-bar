4c68a7d03d39f7d29f8fde2f8a522e47a6bc75d7
Adil Mae <itsadilhadid@gmail.com>
2024-08-12 03:19:10 +0100

    Signed-off-by: Adil Mae <itsadilhadid@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { ShowPageComponent } from './show-page/show-page.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'show', component: ShowPageComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+
   { path: 'profile', component: ProfileComponent },
 ];
